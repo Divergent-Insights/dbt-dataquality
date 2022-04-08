55676f27d1a5bac181219a8e8826f567e2ca82b3
Wilson Urdaneta <wilson.urdaneta@divergentinsights.com.au>
2022-04-09 06:05:46 +0800

    Enhancing sources details logic

--- models/sources/sources_details.sql
+++ models/sources/sources_details.sql
@@ -5,9 +5,10 @@ select distinct
     sf.snapshotted_at,
     sf.status,
     case
-        when (sf.status = 'error') then 2
-        when (sf.status = 'pass') then 1
-        else 0
+        when (sf.status = 'error') then 100
+        when (sf.status = 'warn') then 50
+        when (sf.status = 'pass') then 0
+        else -1
     end as status_code,
     sfm.database as source_database,
     sfm.schema as source_schema,
