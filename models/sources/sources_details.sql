bdbb5e15df4fa14362b6c4c3b17a5d0312f1f894
Wilson Urdaneta <wilson.urdaneta@divergentinsights.com.au>
2022-04-08 13:46:09 +0800

    Adding missing name column on sources manifest

--- models/sources/sources_details.sql
+++ models/sources/sources_details.sql
@@ -1,12 +1,18 @@
-select
-    sf.*,
-    sfm.database,
-    sfm.schema,
-    sfm.description,
-    sfm.loader,
+select distinct
     sfm.source_name,
     sfm.source_description,
-    sfm.package_name,
-    sfm.loaded_at_field
+    sfm.loader,
+    sf.snapshotted_at,
+    sf.status,
+    sfm.database,
+    sfm.schema as source,
+    sfm.name as table_name,
+    sfm.description table_description,
+    sf.freshness_warn_count,
+    sf.freshness_warn_period,
+    sf.freshness_error_count,
+    sf.freshness_error_period,
+    sf.freshness_filter
 from {{ ref('raw_source_freshness') }} sf
 left join {{ ref('raw_source_freshness_manifest') }} sfm on sf.unique_id = sfm.unique_id
+where sf.payload_timestamp_utc >= (select max(payload_timestamp_utc) from my_database.my_schema.raw_source_freshness)
