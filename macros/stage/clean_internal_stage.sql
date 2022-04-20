3842f410b6b72cb58249839688215153f206a4ae
Wilson Vaquen <wilson.vaquen@woodside.com.au>
2022-04-20 14:30:49 +0800

    Adding dry run option

--- macros/stage/clean_internal_stage.sql
+++ macros/stage/clean_internal_stage.sql
@@ -1,12 +1,16 @@
-{% macro clean_internal_stage() %}
+{% macro clean_internal_stage(dry_run=False) %}
 
     {% do log("clean_internal_stage started", info=True) %}
     {% set config = _get_config() %}
 
     {% set sql %}
         remove @{{ config["database"] }}.{{ config["schema"] }}.{{ config["stage"] }} pattern='.*.*';
-    {% endset %}    
-    {% do run_query(sql) %}
+    {% endset %}
+
+    {% if dry_run %}
+        {% do run_query(sql) %}
+    {% endif %}
+
     {% do log(sql, info=True) %}
 
     {% do log("clean_internal_stage completed", info=True) %}
