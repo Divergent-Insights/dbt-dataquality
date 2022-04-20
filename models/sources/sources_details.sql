a3104786b99dd6faf9064e358217493eb56afd34
Wilson Vaquen <wilson@divergentinsights.com.au>
2022-04-20 17:00:40 +0800

    Fixing minor bug with data types on raw_source_freshness model

--- models/sources/sources_details.sql
+++ models/sources/sources_details.sql
@@ -1,27 +1,24 @@
-with cte as
-(
-    select distinct
-        sfm.source_name,
-        sfm.source_description,
-        sfm.loader,
-        sf.snapshotted_at,
-        sf.status,
-        case
-            when (sf.status = 'error') then 100
-            when (sf.status = 'warn') then 50
-            when (sf.status = 'pass') then 0
-            else -1
-        end as status_code,
-        sfm.database as source_database,
-        sfm.schema as source_schema,
-        sfm.name as table_name,
-        sfm.description table_description,
-        (   
-            'warn: ' || ifnull('after' || sf.freshness_warn_count::string || ' ' || sf.freshness_warn_period , 'undefined') || ', ' ||
-            'error: ' || ifnull('after' || sf.freshness_error_count::string || ' ' || sf.freshness_error_period , 'undefined')
-        ) as freshness_check,        
-        sf.freshness_filter
-    from {{ ref('raw_source_freshness') }} sf
-    left join {{ ref('raw_source_freshness_manifest') }} sfm on sf.unique_id = sfm.unique_id
-    where sf.payload_timestamp_utc >= (select max(payload_timestamp_utc) from my_database.my_schema.raw_source_freshness)
-)
+select distinct
+    sfm.source_name,
+    sfm.source_description,
+    sfm.loader,
+    sf.snapshotted_at,
+    sf.status,
+    case
+        when (sf.status = 'error') then 100
+        when (sf.status = 'warn') then 50
+        when (sf.status = 'pass') then 0
+        else -1
+    end as status_code,
+    sfm.database as source_database,
+    sfm.schema as source_schema,
+    sfm.name as table_name,
+    sfm.description table_description,
+    (   
+        'warn: ' || ifnull('after' || sf.freshness_warn_count::string || ' ' || sf.freshness_warn_period , 'undefined') || ', ' ||
+        'error: ' || ifnull('after' || sf.freshness_error_count::string || ' ' || sf.freshness_error_period , 'undefined')
+    ) as freshness_check,
+    sf.freshness_filter
+from {{ ref('raw_source_freshness') }} sf
+left join {{ ref('raw_source_freshness_manifest') }} sfm on sf.unique_id = sfm.unique_id
+where sf.payload_timestamp_utc >= (select max(payload_timestamp_utc) from {{ ref('raw_source_freshness') }})
