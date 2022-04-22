a7feaa39fef6bc3f053b23238a5238ad4aa1b075
Wilson Urdaneta <wilson.urdaneta@divergentinsights.com.au>
2022-04-23 03:48:17 +0800

    Adding tests details models - preliminary version

---
+++ models/tests/tests_details.sql
@@ -0,0 +1,13 @@
+select distinct
+    tm.database,
+    tm.column_name,
+    t.status,
+    case
+        when (t.status = 'error') then 100
+        when (t.status = 'fail') then 50
+        when (t.status = 'pass') then 0
+        else -1
+    end as status_code,
+from {{ ref('raw_tests') }} t
+left join {{ ref('raw_tests_manifest') }} tm on t.unique_id = tm.unique_id
+where t.payload_timestamp_utc >= (select max(payload_timestamp_utc) from {{ ref('raw_source_freshness') }})
